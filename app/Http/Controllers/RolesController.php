6ca4189b5f0b26bfd5c790f298073a83342ff70a
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 02:20:45 -0400

    Create the controller with the api endpoints for the roles table

---
+++ app/Http/Controllers/RolesController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $roles = Role::latest()->paginate(25);
+
+        return $roles;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $role = Role::create($request->all());
+
+        return response()->json($role, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $role = Role::findOrFail($id);
+
+        return $role;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $role = Role::findOrFail($id);
+        $role->update($request->all());
+
+        return response()->json($role, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Role::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
