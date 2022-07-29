f05a6517d94f0beb51366f58f50aeaf6e97b127e
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 03:41:30 -0400

    Create the migration for the areas model

---
+++ database/migrations/2022_07_29_073706_create_areas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name')->collation('utf8mb4_bin');
+            $table->string('description')->collation('utf8mb4_bin')->nullable();
+            $table->boolean('availability')->default(true);
+            $table->float('price');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('areas');
+    }
+}
