d69216c590412c08a4c47a779e5642603d69363c
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 01:58:17 -0400

    Create the controller for the logs model

---
+++ app/Http/Controllers/LogsController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Models\Log;
+use Illuminate\Http\Request;
+
+class LogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $logs = Log::latest()->paginate(25);
+
+        return $logs;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $log = Log::create($request->all());
+
+        return response()->json($log, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $log = Log::findOrFail($id);
+
+        return $log;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $log = Log::findOrFail($id);
+        $log->update($request->all());
+
+        return response()->json($log, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Log::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
