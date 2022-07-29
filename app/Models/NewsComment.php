ecadec1eef338372613ba89a4c5fb108ff6f5520
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 02:54:54 -0400

    Create the model for the news comments

---
+++ app/Models/NewsComment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsComment extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'news_comments';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['comment_text', 'new_id', 'user_id'];
+
+    
+}
