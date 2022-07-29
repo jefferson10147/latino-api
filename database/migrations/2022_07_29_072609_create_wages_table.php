0a1171147728c4700799b8db4763d16aeb26cbbf
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 03:34:20 -0400

    Define the migration for the wages model

---
+++ database/migrations/2022_07_29_072609_create_wages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedFloat('total');
+            $table->string('description')->collation('utf8mb4_bin')->nullable();
+            $table->unsignedInteger('month');
+            $table->unsignedInteger('year');
+            $table->boolean('status');
+            $table->unsignedInteger('user_id');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('wages');
+    }
+}
