144415e630403cfda231b206f1c9a60aa5029827
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 03:55:33 -0400

    Define the migration for the reservations table

---
+++ database/migrations/2022_07_29_074812_create_rerservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRerservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rerservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->boleean('approved')->default(false);
+            $table->text('description')->collation('utf8mb4_bin');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rerservations');
+    }
+}
