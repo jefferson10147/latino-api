0e9132ecc812c3454d2e374c4588bf3be367fcf7
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 03:05:29 -0400

    Create the migration for the associate members table

---
+++ database/migrations/2022_07_29_070030_create_associate_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssociateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('associate_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name')->collation('utf8mb4_bin');
+            $table->string('last_name')->collation('utf8mb4_bin');
+            $table->integer('dni')->nullable();
+            $table->string('relationship')->collation('utf8mb4_bin');
+            $table->unsignedBigInteger('user_id');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('associate_members');
+    }
+}
