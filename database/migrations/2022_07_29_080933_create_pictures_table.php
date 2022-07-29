9958aacab309ac15c4627bf6eee2971c3c9eccda
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 04:15:41 -0400

    Define the migration for the pictures model

---
+++ database/migrations/2022_07_29_080933_create_pictures_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name')->collation('utf8mb4_bin');
+            $table->string('url');
+            $table->unsignedInteger('new_id')->nullable();
+            $table->foreign('new_id')->references('id')->on('news')->onDelete('cascade');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedInteger('area_id')->nullable();
+            $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pictures');
+    }
+}
