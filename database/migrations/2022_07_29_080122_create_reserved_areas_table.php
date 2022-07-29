c1d21815a58e54c843fb37e88624ef76a1d3ae8c
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 04:06:05 -0400

    Define the migrations for the transition table

---
+++ database/migrations/2022_07_29_080122_create_reserved_areas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservedAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserved_areas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('reservation_id')->nullable();
+            $table->foreign('reservation_id')->references('id')->on('rerservations');
+            $table->integer('area_id')->nullable();
+            $table->foreign('area_id')->references('id')->on('areas');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reserved_areas');
+    }
+}
