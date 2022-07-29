7973fba5dd6e300e2c71fe2a6ec591aa408d4a77
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 02:32:25 -0400

    Create the migration for the news model

---
+++ database/migrations/2022_07_29_062709_create_news_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title')->collation('utf8mb4_bin');
+            $table->text('body')->collation('utf8mb4_bin');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('news');
+    }
+}
