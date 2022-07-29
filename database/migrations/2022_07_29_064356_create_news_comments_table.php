51b90cea9789866a059631f0f863db984d830455
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 02:55:33 -0400

    Create the migration for the news_comments table model

---
+++ database/migrations/2022_07_29_064356_create_news_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('comment_text')->collation('utf8mb4_bin');
+            $table->unsignedBigInteger('new_id');
+            $table->unsignedInteger('user_id');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('news_comments');
+    }
+}
