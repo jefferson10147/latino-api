6181149a9b72868cffd8f598e5361a6902487699
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-29 03:42:06 -0400

    Define the endpoints for the areas table

---
+++ app/Http/Controllers/AreasController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Models\Area;
+use Illuminate\Http\Request;
+
+class AreasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $areas = Area::latest()->paginate(25);
+
+        return $areas;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $area = Area::create($request->all());
+
+        return response()->json($area, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $area = Area::findOrFail($id);
+
+        return $area;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $area = Area::findOrFail($id);
+        $area->update($request->all());
+
+        return response()->json($area, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Area::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
