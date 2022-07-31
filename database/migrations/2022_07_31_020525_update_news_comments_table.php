7faacb25379119993dd62b5d2f158e94ebda36da
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-30 23:33:43 -0400

    Add the foreigns keys for the news comments table

---
+++ database/migrations/2022_07_31_020525_update_news_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news_comments', function (Blueprint $table) {
+            $table->foreign('new_id')->references('id')->on('news')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
