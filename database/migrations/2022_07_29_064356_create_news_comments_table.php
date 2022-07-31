edcb21f5746cad5850883beeb234493f82c596cf
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-30 22:23:43 -0400

    Update the data type from the news comments table

--- database/migrations/2022_07_29_064356_create_news_comments_table.php
+++ database/migrations/2022_07_29_064356_create_news_comments_table.php
@@ -16,7 +16,7 @@ class CreateNewsCommentsTable extends Migration
         Schema::create('news_comments', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->text('comment_text')->collation('utf8mb4_bin');
+            $table->tinyText('comment_text')->collation('utf8mb4_bin');
             $table->unsignedInteger('new_id');
             // $table->foreign('new_id')->references('id')->on('news')->onDelete('cascade');
             $table->unsignedInteger('user_id');
