9856a45a0a81ad7479c34e97a00f3bff36ce5e9d
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-31 00:00:01 -0400

    Add the foreigns keys for the reserved areas table

---
+++ database/migrations/2022_07_31_035826_update_reserved_areas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reserved_areas', function (Blueprint $table) {
+            $table->foreign('reservation_id')->references('id')->on('rerservations')->onDelete('cascade');
+            $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
