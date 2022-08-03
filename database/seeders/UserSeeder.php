d6f3338e4b8cb3281fccb852d8803f92fc3416e9
Jefferson <jeffersonmontilla.98@gmail.com>
2022-08-03 00:54:32 -0400

    Create the seeder for the users table

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'role_id' => 1,
+            'name' => 'Scarlett',
+            'last_name' => 'Johansson',
+            'dni' => Str::random(8),
+            'email' => Str::random(10).'@gmail.com',
+            'address' => Str::random(10),
+            'membership_number' => 1234,
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'birthdate' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'role_id' => 2,
+            'name' => 'Cristiano',
+            'last_name' => 'Ronaldo',
+            'dni' => Str::random(8),
+            'email' => Str::random(10).'@gmail.com',
+            'address' => Str::random(10),
+            'membership_number' => 5678,
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'birthdate' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
