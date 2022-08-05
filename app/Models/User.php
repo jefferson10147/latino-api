66633a2da10ac7280d538e4790b17c5a30385608
Jefferson <jeffersonmontilla.98@gmail.com>
2022-08-04 22:25:38 -0400

    Feat: update the users module, it should make the join to roles table and get the object

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected $with = ['role'];
+
     public function role()
     {
         return $this->belongsTo(Role::class);
