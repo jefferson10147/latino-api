15df58d017ed9c2a28d1238622cc2b07e1dcfd2f
Jefferson <jeffersonmontilla.98@gmail.com>
2022-08-07 14:32:54 -0400

    Feat: add the user middleware for associated members routes

--- routes/api.php
+++ routes/api.php
@@ -64,11 +64,11 @@ Route::resource('logs', LogsController::class, ['except' => ['create', 'edit']])
 
 Route::middleware(['SessionValidateAdminUser'])->group(function () {
     Route::resource('sport-clubs', SportClubsController::class, ['except' => ['create', 'edit']]);
+    Route::resource('associate-members', AssociateMembersController::class, ['except' => ['create', 'edit']]);
 });
 
 Route::resource('news', NewsController::class, ['except' => ['create', 'edit']]);
 Route::resource('news-comments', NewsCommentsController::class, ['except' => ['create', 'edit']]);
-Route::resource('associate-members', AssociateMembersController::class, ['except' => ['create', 'edit']]);
 Route::resource('wages', WagesController::class, ['except' => ['create', 'edit']]);
 Route::resource('areas', AreasController::class, ['except' => ['create', 'edit']]);
 Route::resource('reservations', ReservationsController::class, ['except' => ['create', 'edit']]);
