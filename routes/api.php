a6343312309d94e1b24282295dde162d089f1222
Jefferson <jeffersonmontilla.98@gmail.com>
2022-08-07 14:36:54 -0400

    Delete the reminder on api.php

--- routes/api.php
+++ routes/api.php
@@ -32,29 +32,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/*
-    RUTAS Y METODOS a los que un usuario normal puede acceder:
-
-
-    - GET /api/news: Obtiene todas las noticias
-    - GET /api/news/{id}: Obtiene una noticia por id
-    
-    - GET /api/news-comments: Obtiene todos los comentarios de las noticias
-    - POST /api/news-comments: Crea un comentario de una noticia
-
-    
-    - GET /api/reservations/{id}: Obtiene una reserva por ID (que estan asociadas a un usuario)
-    - POST /api/reservations: Crea una reserva
-    - PATCH /api/reservations/{id}: Actualiza una reserva
-    - DELETE /api/reservations/{id}: Elimina una reserva por ID (que estan asociadas a un usuario)
-
-    - GET /api/associate-members: Obtiene todos los miembros asociados a un usuario
-
-    - GET /api/wages: Obtiene todas las facturas asociadas a su cuenta
-
-    El resto de rutas y metodos en cada una de ellas deben ser accedidas por un usuario administrador
-*/
-
 Route::middleware(['SessionValidateAdmin'])->group(function () {
     Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
     Route::resource('roles', RolesController::class, ['except' => ['create', 'edit']]);
